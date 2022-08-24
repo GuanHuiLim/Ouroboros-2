e9ec6a1c5b197d1fe4f5c3a8975000c74432ca42
Chua Teck Lee <C_tecklee@Hotmail.com>
2022-08-24 19:42:50 +0800

    basic prefab scene and controller set up

---
+++ Editor/src/Ouroboros/Scene/PrefabSceneController.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SceneManagement/include/SceneManager.h>
+#include "PrefabScene.h"
+
+namespace oo
+{
+    class PrefabSceneController
+    {
+    public:
+        PrefabSceneController(SceneManager& sceneManager);
+
+        Scene::go_ptr RequestForPrefab(std::string const& filepath);
+
+    private:
+        SceneManager& m_sceneManager;
+        std::weak_ptr<PrefabScene> m_prefabScene = {};
+    };
+}
