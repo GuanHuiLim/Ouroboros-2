2b6f7e438956d127c5d6ba48fea18fc21fddabb4
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2022-08-24 14:21:15 +0800

    nested object insepctor

--- Editor/src/App/Editor/UI/Object Editor/Inspector.cpp
+++ Editor/src/App/Editor/UI/Object Editor/Inspector.cpp
@@ -21,65 +21,77 @@
 #include <glm/gtc/type_ptr.hpp>
 Inspector::Inspector()
 {
-	m_InspectorUI[UI_RTTRType::UItypes::BOOL_TYPE] = [](std::string& name, rttr::variant& v, bool& edited) 
+	m_InspectorUI[UI_RTTRType::UItypes::BOOL_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		bool value = v.get_value<bool>();
 		edited = ImGui::Checkbox(name.c_str(),&value);
-		if (edited) v = value;
+		if (edited) { v = value; endEdit = true; };
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::STRING_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::STRING_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<std::string>();
 		edited = ImGui::InputText(name.c_str(), &value);
-		if (edited) v = value;
+		if (edited) { v = value; };
+		endEdit = ImGui::IsItemDeactivatedAfterEdit();
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC2_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::VEC2_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<glm::vec2>();
 		edited = ImGui::DragFloat2(name.c_str(), glm::value_ptr(value));
-		if (edited) v = value;
+		if (edited) { v = value; };
+		endEdit = ImGui::IsItemDeactivatedAfterEdit();
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::VEC3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<glm::vec3>();
 		edited = ImGui::DragFloat3(name.c_str(), glm::value_ptr(value));
-		if (edited) v = value;
+		if (edited) { v = value; };
+		endEdit = ImGui::IsItemDeactivatedAfterEdit();
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::VEC4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<glm::vec4>();
 		edited = ImGui::DragFloat4(name.c_str(), glm::value_ptr(value));
-		if (edited) v = value;
+		if (edited) { v = value; };
+		endEdit = ImGui::IsItemDeactivatedAfterEdit();
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::UUID_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::UUID_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<UUID>();
 		ImGui::PushItemFlag(ImGuiItemFlags_::ImGuiItemFlags_Disabled, true);
 		ImGui::InputScalarN(name.c_str(),ImGuiDataType_::ImGuiDataType_U64, &value,1);//read only
 		ImGui::PopItemFlag();
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::MAT3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::MAT3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<glm::mat3>();
 		edited |= ImGui::DragFloat3(name.c_str(), glm::value_ptr(value[0]));
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 		edited |= ImGui::DragFloat3("##2", glm::value_ptr(value[1]));
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 		edited |= ImGui::DragFloat3("##3", glm::value_ptr(value[2]));
-		if (edited) v = value;
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
+		if (edited) { v = value; };
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::MAT4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::MAT4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<glm::mat4>();
 		edited |= ImGui::DragFloat4(name.c_str(), glm::value_ptr(value[0]));
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 		edited |= ImGui::DragFloat4("##2", glm::value_ptr(value[1]));
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 		edited |= ImGui::DragFloat4("##3", glm::value_ptr(value[2]));
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 		edited |= ImGui::DragFloat4("##4", glm::value_ptr(value[3]));
-		if (edited) v = value;
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
+		if (edited) { v = value; };
 	};
-	m_InspectorUI[UI_RTTRType::UItypes::QUAT_TYPE] = [](std::string& name, rttr::variant& v, bool& edited)
+	m_InspectorUI[UI_RTTRType::UItypes::QUAT_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
 	{
 		auto value = v.get_value<quaternion>().value;
 		edited = ImGui::DragFloat4(name.c_str(), glm::value_ptr(value));
-		if (edited) v = value;
+		if (edited) { v = value; };
+		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
 	};
 }
 
--- Editor/src/App/Editor/UI/Object Editor/Inspector.h
+++ Editor/src/App/Editor/UI/Object Editor/Inspector.h
@@ -28,19 +28,23 @@ public:
 private:
 	template <typename Component>
 	void DisplayComponent(oo::GameObject& gameobject);
-	std::unordered_map<UI_RTTRType::UItypes, std::function<void(std::string& name,rttr::variant & v, bool & edited)>> m_InspectorUI;
-
+	void DisplayNestedComponent(rttr::type class_type,rttr::variant& value, bool& edited, bool& endEdit);
 private:
+	std::unordered_map<UI_RTTRType::UItypes, std::function<void(std::string& name,rttr::variant & v, bool & edited , bool& endEdit)>> m_InspectorUI;
 	bool m_showReadonly = false;
 };
 
 template<typename Component>
 inline void Inspector::DisplayComponent(oo::GameObject& gameobject)
 {
+	static rttr::variant pre_edited;
+	
 	if (gameobject.HasComponent<Component>() == false)
 		return;
+	
 	auto& component = gameobject.GetComponent<Component>();
 	rttr::type type = component.get_type();
+	
 	ImGui::Text(type.get_name().data());
 	ImGui::Separator();
 	ImGui::PushID(type.get_id());
@@ -57,29 +61,69 @@ inline void Inspector::DisplayComponent(oo::GameObject& gameobject)
 			continue;
 
 		auto iter = m_InspectorUI.find(ut->second);
+		//special cases
 		if (iter == m_InspectorUI.end())
+		{
+			//nested variables & arrays
+			if (prop_type.is_array())//array
+			{
+				ASSERT_MSG(true, "AYO u doing illegal shit");
+			}
+			else if (prop_type.is_class())//nested
+			{
+				rttr::variant value = prop.get_value(component);
+				bool edited = false;
+				bool end_edit = false;
+				DisplayNestedComponent(prop_type, value, edited, end_edit);
+				if (end_edit)
+				{
+					//undo redo command
+					oo::CommandStackManager::AddCommand(new oo::Component_ActionCommand<Component>
+						(pre_edited, value, prop, gameobject.GetInstanceID()));
+					pre_edited.clear();//reset this variant
+				}
+				if (edited == true)
+				{
+					if (pre_edited.is_valid() == false)//if the variant is empty that means it is ready to hold data
+						pre_edited = prop.get_value(component);
+					//set value to component
+					prop.set_value(component, value);
+				}
+			}
 			continue;
+		}
 
 		rttr::variant v = prop.get_value(component);
 		bool set_value = false;
+		bool end_edit = false; //when the item is let go
 		std::string name = prop.get_name().data();
 		
 		if (propReadonly)
 		{
 			ImGui::PushItemFlag(ImGuiItemFlags_::ImGuiItemFlags_Disabled,true);
 			ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_FrameBg, ImGui_StylePresets::disabled_color);
-			iter->second(name, v, set_value);
+			iter->second(name, v, set_value, end_edit);
 			ImGui::PopStyleColor();
 			ImGui::PopItemFlag();
 		}
 		else
 		{
-			iter->second(name, v, set_value);
-			if (set_value == true)
+			//tracks the currently edited item.
+			//the item will get reseted once the edit ends.
+			//the reset must happen else something bad will happen
+			
+			iter->second(name, v, set_value, end_edit);
+			if (end_edit)
 			{
 				//undo redo command
 				oo::CommandStackManager::AddCommand(new oo::Component_ActionCommand<Component>
-					(prop.get_value(component), v, prop, gameobject.GetInstanceID()));
+					(pre_edited, v, prop, gameobject.GetInstanceID()));
+				pre_edited.clear();//reset this variant
+			}
+			if (set_value == true)
+			{
+				if(pre_edited.is_valid() == false)//if the variant is empty that means it is ready to hold data
+					pre_edited = prop.get_value(component);
 				//set value to component
 				prop.set_value(component,v);
 			}
@@ -88,4 +132,71 @@ inline void Inspector::DisplayComponent(oo::GameObject& gameobject)
 	}
 	ImGui::PopID();
 	ImGui::Separator();
+}
+void Inspector::DisplayNestedComponent(rttr::type class_type, rttr::variant& value, bool& edited, bool& endEdit)
+{
+	ImGui::Dummy({ 5.0f,0 });
+	ImGui::SameLine();
+	ImGui::BeginGroup();
+	ImGui::Text(class_type.get_name().data());
+	ImGui::Separator();
+	ImGui::PushID(class_type.get_id());
+
+	for (rttr::property prop : class_type.get_properties())
+	{
+		bool propReadonly = prop.is_readonly();
+		if (propReadonly && m_showReadonly == false)
+			continue;
+
+		rttr::type prop_type = prop.get_type();
+
+		auto ut = UI_RTTRType::types.find(prop_type.get_id());
+		if (ut == UI_RTTRType::types.end())
+			continue;
+
+		auto iter = m_InspectorUI.find(ut->second);
+		if (iter == m_InspectorUI.end())
+		{
+			//nested variables & arrays
+			if (prop_type.is_array())//array
+			{
+				ASSERT_MSG(true, "AYO u doing illegal shit");
+			}
+			else if (prop_type.is_class())//nested
+			{
+				ASSERT_MSG(true, "AYO u doing illegal shit");
+				//rttr::variant value = prop.get_value(component);
+
+			}
+			continue;
+		}
+
+		rttr::variant v = prop.get_value(value);
+		bool set_value = false;
+		bool end_edit = false; //when the item is let go
+		std::string name = prop.get_name().data();
+
+		if (propReadonly)
+		{
+			ImGui::PushItemFlag(ImGuiItemFlags_::ImGuiItemFlags_Disabled, true);
+			ImGui::PushStyleColor(ImGuiCol_::ImGuiCol_FrameBg, ImGui_StylePresets::disabled_color);
+			iter->second(name, v, set_value, end_edit);
+			ImGui::PopStyleColor();
+			ImGui::PopItemFlag();
+		}
+		else
+		{
+			iter->second(name, v, set_value, end_edit);
+			if (end_edit)
+				endEdit = true;
+			if (set_value == true)
+			{
+				edited = true;
+				prop.set_value(value, v);//set value to variant
+			}
+		}
+		ImGui::Dummy({ 0,5.0f });
+	}
+	ImGui::PopID();
+	ImGui::EndGroup();
 }
