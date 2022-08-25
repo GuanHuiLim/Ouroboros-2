e9b465115db9c9197ff4e16ab4bcf97171041859
Chua Teck Lee <C_tecklee@Hotmail.com>
2022-08-25 16:24:58 +0800

    fixed rendering bug

--- Editor/vendor/vulkan/OO_Vulkan/src/MathCommon.h
+++ Editor/vendor/vulkan/OO_Vulkan/src/MathCommon.h
@@ -3,6 +3,9 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
+// TL test
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
