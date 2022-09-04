0e7b4b2ec26043c68d08631b22dab5531e5d03e0
Chua Teck Lee <C_tecklee@Hotmail.com>
2022-09-04 12:39:35 +0800

    return hash to its original state (hopefully no more problems)

--- Editor/src/Utility/Hash.h
+++ Editor/src/Utility/Hash.h
@@ -27,10 +27,31 @@ struct StringHash
 {
 public:
     using size_type = std::uint32_t;
+    static constexpr size_type  GenerateFNV1aHash(const char* str)
+    {
+        // Also C++ does not like static constexpr
+        constexpr size_type FNV_PRIME = 16777619u;
+        constexpr size_type OFFSET_BASIS = 2166136261u;
 
-    constexpr static size_t const_strlen(char const* s);
-    static const size_type  GenerateFNV1aHash(char const* str);
-    static const size_type GenerateFNV1aHash(std::string_view string);
+        const size_t length = const_strlen(str) + 1;
+        size_type hash = OFFSET_BASIS;
+        for (size_t i = 0; i < length; ++i)
+        {
+            hash ^= *str++;
+            hash *= FNV_PRIME;
+        }
+        return hash;
+    }
+    static constexpr size_type GenerateFNV1aHash(std::string_view string)
+    {
+        return GenerateFNV1aHash(string.data());
+    }
+    static constexpr size_t const_strlen(const char* s)
+    {
+        size_t size = 0;
+        while (s[size]) { size++; };
+        return size;
+    }
 
 private:
     size_type computedHash;
@@ -38,27 +59,58 @@ private:
 public:
     StringHash(StringHash const& other) = default;
 
-    StringHash(size_type hash) noexcept;
-    StringHash(char const* s) noexcept;
-    StringHash(std::string_view s) noexcept;
-    StringHash(std::string const& s) noexcept;
+    //constexpr StringHash(size_type hash) noexcept;
+    //constexpr StringHash(char const* s) noexcept;
+    //constexpr StringHash(std::string_view s) noexcept;
+    //constexpr StringHash(std::string const& s) noexcept;
 
-    operator size_type() const noexcept;
+    //constexpr operator size_type() const noexcept;
+
+
+    constexpr StringHash(size_type hash) noexcept
+        : computedHash(hash)
+    {
+    }
+
+    constexpr StringHash(const char* s) noexcept
+        : computedHash(0)
+    {
+        computedHash = GenerateFNV1aHash(s);
+    }
+
+    constexpr StringHash(std::string_view s) noexcept
+        : computedHash(0)
+    {
+        computedHash = GenerateFNV1aHash(s.data());
+    }
+
+    constexpr StringHash(std::string const& s) noexcept
+        : computedHash(0)
+    {
+        computedHash = GenerateFNV1aHash(s.data());
+    }
+
+    constexpr operator size_type() const noexcept
+    {
+        return computedHash;
+    }
+
+
     //constexpr bool operator==(StringHash const& other) const noexcept;
     //constexpr bool operator<(StringHash const& other) const noexcept;// { return computedHash < other.computedHash; }
 
     auto operator<=>(StringHash const& other) const = default;
 };
 
-//// hashing overload for StringHash with std lib
-//namespace std
-//{
-//    template<>
-//    struct hash<StringHash>
-//    {
-//        std::size_t operator() (StringHash const& newhash) const
-//        {
-//            return hash<std::size_t>()((std::uint32_t)newhash);
-//        }
-//    };
-//}
+// hashing overload for StringHash with std lib
+namespace std
+{
+    template<>
+    struct hash<StringHash>
+    {
+        std::size_t operator() (StringHash const& newhash) const
+        {
+            return hash<std::size_t>()((std::uint32_t)newhash);
+        }
+    };
+}
