b59e48c69c8d7e56cd3859d04876f2145e64d636
Chua Teck Lee <C_tecklee@Hotmail.com>
2022-09-18 11:44:45 +0800

    fixed merge problems

--- Editor/src/App/Editor/UI/Object Editor/Inspector.cpp
+++ Editor/src/App/Editor/UI/Object Editor/Inspector.cpp
@@ -26,7 +26,7 @@
 #include <Ouroboros/Scripting/ScriptComponent.h>
 #include <Ouroboros/Scripting/ScriptSystem.h>
 #include <Ouroboros/Scripting/ScriptManager.h>
-//#include <Ouroboros/Physics/RigidbodyComponent.h>
+#include <Ouroboros/Physics/RigidbodyComponent.h>
 #include <Ouroboros/Vulkan/RendererComponent.h>
 
 #include <glm/gtc/type_ptr.hpp>
@@ -34,85 +34,6 @@
 Inspector::Inspector()
 	:m_AddComponentButton("Add Component", false, {200,50},ImGui_StylePresets::disabled_color,ImGui_StylePresets::prefab_text_color)
 {
-	m_InspectorUI[UI_RTTRType::UItypes::BOOL_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		bool value = v.get_value<bool>();
-		edited = ImGui::Checkbox(name.c_str(),&value);
-		if (edited) { v = value; endEdit = true; };
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::FLOAT_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<float>();
-		edited = ImGui::DragFloat(name.c_str(), &value);
-		if (edited) { v = value; };
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::STRING_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.to_string();
-		edited = ImGui::InputText(name.c_str(), &value);
-		if (edited) { v = value; };
-		endEdit = ImGui::IsItemDeactivatedAfterEdit();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC2_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<glm::vec2>();
-		edited = ImGui::DragFloat2(name.c_str(), glm::value_ptr(value));
-		if (edited) { v = value; };
-		endEdit = ImGui::IsItemDeactivatedAfterEdit();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<glm::vec3>();
-		edited = ImGui::DragFloat3(name.c_str(), glm::value_ptr(value));
-		if (edited) { v = value; };
-		endEdit = ImGui::IsItemDeactivatedAfterEdit();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::VEC4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<glm::vec4>();
-		edited = ImGui::DragFloat4(name.c_str(), glm::value_ptr(value));
-		if (edited) { v = value; };
-		endEdit = ImGui::IsItemDeactivatedAfterEdit();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::UUID_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<UUID>();
-		ImGui::PushItemFlag(ImGuiItemFlags_::ImGuiItemFlags_Disabled, true);
-		ImGui::InputScalarN(name.c_str(),ImGuiDataType_::ImGuiDataType_U64, &value,1);//read only
-		ImGui::PopItemFlag();
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::MAT3_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<glm::mat3>();
-		edited |= ImGui::DragFloat3(name.c_str(), glm::value_ptr(value[0]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		edited |= ImGui::DragFloat3("##2", glm::value_ptr(value[1]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		edited |= ImGui::DragFloat3("##3", glm::value_ptr(value[2]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		if (edited) { v = value; };
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::MAT4_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<glm::mat4>();
-		edited |= ImGui::DragFloat4(name.c_str(), glm::value_ptr(value[0]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		edited |= ImGui::DragFloat4("##2", glm::value_ptr(value[1]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		edited |= ImGui::DragFloat4("##3", glm::value_ptr(value[2]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		edited |= ImGui::DragFloat4("##4", glm::value_ptr(value[3]));
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-		if (edited) { v = value; };
-	};
-	m_InspectorUI[UI_RTTRType::UItypes::QUAT_TYPE] = [](std::string& name, rttr::variant& v, bool& edited, bool& endEdit)
-	{
-		auto value = v.get_value<quaternion>().value;
-		edited = ImGui::DragFloat4(name.c_str(), glm::value_ptr(value));
-		if (edited) { v = value; };
-		endEdit |= ImGui::IsItemDeactivatedAfterEdit();
-	};
 }
 
 void Inspector::Show()
@@ -190,9 +111,11 @@ void Inspector::DisplayAllComponents(oo::GameObject& gameobject)
 	DisplayComponent<oo::SphereCollider>(gameobject);
 	DisplayComponent<oo::BoxCollider>(gameobject);
 
-	//DisplayComponent<oo::RigidbodyComponent>(gameobject);
+	DisplayComponent<oo::RigidbodyComponent>(gameobject);
 	DisplayComponent<oo::GameObjectDebugComponent>(gameobject);
 	DisplayComponent<oo::MeshRendererComponent>(gameobject);
+
+	DisplayComponent<oo::DeferredComponent>(gameobject);
 	DisplayScript(gameobject);
 }
 void Inspector::DisplayAddComponents(oo::GameObject& gameobject, float x , float y)
@@ -210,7 +133,8 @@ void Inspector::DisplayAddComponents(oo::GameObject& gameobject, float x , float
 		ImGui::BeginListBox("##AddComponents", { x,y });
 		ImGui::BeginChild("##aclistboxchild", { x - 10 ,y * 0.70f },true);
 		selected |= AddComponentSelectable<oo::GameObjectComponent>(gameobject);
-		
+
+		selected |= AddComponentSelectable<oo::RigidbodyComponent>(gameobject);
 		//selected |= AddComponentSelectable<oo::ColliderComponent>(gameobject);
 		selected |= AddComponentSelectable<oo::BoxCollider>(gameobject);
 		selected |= AddComponentSelectable<oo::SphereCollider>(gameobject);
@@ -219,8 +143,6 @@ void Inspector::DisplayAddComponents(oo::GameObject& gameobject, float x , float
 		selected |= AddComponentSelectable<oo::MeshRendererComponent>(gameobject);
 
 		//selected |= AddComponentSelectable<oo::DeferredComponent>(gameobject);
-
-		//selected |= AddComponentSelectable<oo::RigidbodyComponent>(gameobject);
 
 		selected |= AddScriptsSelectable(gameobject);
 
