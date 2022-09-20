71cd55373a06591311fd11199576717be1a721b9
Jamie Kong <j.kong@digipen.edu>
2022-09-20 16:59:18 +0800

    DEFINE NOMINMAX

--- Editor/vendor/vulkan/OO_Vulkan/src/VulkanRenderer.cpp
+++ Editor/vendor/vulkan/OO_Vulkan/src/VulkanRenderer.cpp
@@ -1,4 +1,6 @@
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include "VulkanRenderer.h"
 
 #include "MathCommon.h"
--- Editor/vendor/vulkan/OO_Vulkan/src/VulkanUtils.cpp
+++ Editor/vendor/vulkan/OO_Vulkan/src/VulkanUtils.cpp
@@ -1,6 +1,8 @@
 
 #if defined(_WIN32)
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 
