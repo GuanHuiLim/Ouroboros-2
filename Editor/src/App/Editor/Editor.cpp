d9535915afa24cac8205f894ce2885686372d3c4
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2022-09-26 16:11:14 +0800

    fixed issue

--- Editor/src/App/Editor/Editor.cpp
+++ Editor/src/App/Editor/Editor.cpp
@@ -35,6 +35,9 @@ Editor::Editor()
 	Serializer::Init();//runs the init function
 	Serializer::InitEvents();
 	oo::CommandStackManager::InitEvents();
+
+	
+
 	ImGuiManager::Create("Hierarchy", true, ImGuiWindowFlags_MenuBar, [this] {this->m_hierarchy.Show(); });
 	ImGuiManager::Create("Inspector", true, ImGuiWindowFlags_MenuBar, [this] {this->m_inspector.Show(); });
 	ImGuiManager::Create("FileBrowser", true, ImGuiWindowFlags_MenuBar, [this] {this->m_fileBrowser.Show(); });
@@ -68,6 +71,7 @@ void Editor::Update()
 	static bool b = [this]() 
 	{
 		m_styleEditor.InitStyle(); 
+		m_toolbar.InitAssets();
 		return true; 
 	}();
 	ImGui::DockSpaceOverViewport(ImGui::GetWindowViewport(), ImGuiDockNodeFlags_PassthruCentralNode);
