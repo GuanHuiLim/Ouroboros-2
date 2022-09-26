eee92eb6875bf8db2dde081660aa51b86f3324ec
Lim Guan Hui <l.guanhui@digipen.edu.sg>
2022-09-27 02:24:59 +0800

    skin mesh and skin mesh bone coponent and related systems

---
+++ Editor/src/Ouroboros/Vulkan/SkinRendererSystem.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "SkinRendererSystem.h"
+
+namespace oo
+{
+	SkinMeshRendererSystem::SkinMeshRendererSystem(GraphicsWorld* graphicsWorld)
+		: m_graphicsWorld{ graphicsWorld }
+	{
+	}
+	void SkinMeshRendererSystem::Init()
+	{
+	}
+	void SkinMeshRendererSystem::Run(Ecs::ECSWorld* world)
+	{
+		static Ecs::Query skin_mesh_query = []()
+		{
+			Ecs::Query query;
+			return query.with<SkinMeshRendererComponent, TransformComponent>().build();
+		}();
+		static Ecs::Query skin_bone_mesh_query = []()
+		{
+			Ecs::Query query;
+			return query.with<SkinMeshBoneComponent, TransformComponent>().build();
+		}();
+
+		world->for_each(skin_bone_mesh_query,
+			[&](MeshRendererComponent& m_comp, TransformComponent& transformComp)
+			{
+				//do nothing if transform did not change
+				auto& actualObject = m_graphicsWorld->GetObjectInstance(m_comp.graphicsWorld_ID);
+
+				if (transformComp.HasChanged())
+					actualObject.localToWorld = transformComp.GetGlobalMatrix();
+			});
+
+		world->for_each(skin_mesh_query, 
+			[&](MeshRendererComponent& m_comp, TransformComponent& transformComp)
+			{
+				//do nothing if transform did not change
+				auto& actualObject = m_graphicsWorld->GetObjectInstance(m_comp.graphicsWorld_ID);
+
+				if (transformComp.HasChanged())
+					actualObject.localToWorld = transformComp.GetGlobalMatrix();
+			});
+
+		
+	}
+}
