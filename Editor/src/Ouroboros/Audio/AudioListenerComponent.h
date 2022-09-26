754506f6d8a02213548193020d4b269dd32144fb
Clarence Tay <me@petraller.com>
2022-09-26 16:33:56 +0800

    Audio listener

---
+++ Editor/src/Ouroboros/Audio/AudioListenerComponent.h
@@ -0,0 +1,45 @@
+/************************************************************************************//*!
+\file           AudioListenerComponent.h
+\project        Ouroboros
+\author         Tay Yan Chong Clarence, t.yanchongclarence, 620008720 | code contribution (100%)
+\par            email: t.yanchongclarence\@digipen.edu
+\date           Sep 26, 2022
+\brief          Contains the declaration for the Audio Listener component.
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
+
+#pragma once
+
+#include <rttr/type>
+
+namespace oo
+{
+    class AudioListenerComponent
+    {
+    public:
+        /* --------------------------------------------------------------------------- */
+        /* Getters                                                                     */
+        /* --------------------------------------------------------------------------- */
+
+        [[nodiscard]] inline float GetVolume() const { return volume; }
+
+        /* --------------------------------------------------------------------------- */
+        /* Setters                                                                     */
+        /* --------------------------------------------------------------------------- */
+
+        void SetVolume(float v);
+
+        RTTR_ENABLE();
+
+    private:
+        /* --------------------------------------------------------------------------- */
+        /* Serialised Members                                                          */
+        /* --------------------------------------------------------------------------- */
+
+        float volume = 1;
+    };
+}
