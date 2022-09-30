da22da1b3d8943675ced9cc077685e677ee7a730
Chua Teck Lee <C_tecklee@Hotmail.com>
2022-09-30 19:18:29 +0800

    updated renderer component

--- Editor/src/Ouroboros/Vulkan/RendererComponent.cpp
+++ Editor/src/Ouroboros/Vulkan/RendererComponent.cpp
@@ -27,7 +27,7 @@ namespace oo
 	RTTR_REGISTRATION
 	{
 		using namespace rttr;
-	registration::class_<MeshRendererComponent>("Mesh Renderer")
+		registration::class_<MeshRendererComponent>("Mesh Renderer")
 		.property_readonly("Model Handle", &MeshRendererComponent::model_handle)
 		.property("Albedo", &MeshRendererComponent::albedo_handle)
 		(
@@ -42,5 +42,61 @@ namespace oo
 			metadata(UI_metadata::ASSET_TYPE, static_cast<int>(AssetInfo::Type::Model))
 		)
 		.property("SubmodelID", &MeshRendererComponent::GetSubModelID, &MeshRendererComponent::SetSubModelID);
+	}
+	
+	void MeshRendererComponent::GetModelHandle()
+	{
+		if (mesh_handle.HasData())
+			model_handle = mesh_handle.GetData<ModelData*>()->gfxMeshIndices[submodel_id];
+		else
+			model_handle = 0;
+
+		//model_handle /*= mesh_handle.GetData<ModelData>().gfxMeshIndices.front()*/;
+	}
+	
+	void MeshRendererComponent::SetModelHandle(Asset _asset, uint32_t _submodel_id)
+	{
+		submodel_id = _submodel_id;
+		mesh_handle = _asset;
+
+		model_handle = mesh_handle.GetData<ModelData*>()->gfxMeshIndices[submodel_id];
+	}
+	
+	Asset MeshRendererComponent::GetMesh()
+	{
+		return mesh_handle;
+	}
+	
+	void MeshRendererComponent::SetMesh(Asset _asset)
+	{
+		if (_asset.IsValid())
+		{
+			mesh_handle = _asset;
+			model_handle = mesh_handle.GetData<ModelData*>()->gfxMeshIndices[submodel_id];
+		}
+		if (albedo_handle.IsValid())
+		{
+			albedoID = albedo_handle.GetData<uint32_t>();
+		}
+		if (normal_handle.IsValid())
+		{
+			normalID = normal_handle.GetData<uint32_t>();
+		}
+	}
+	
+	int MeshRendererComponent::GetSubModelID()
+	{
+		return submodel_id;
+	}
+	
+	void MeshRendererComponent::SetSubModelID(int id)
+	{
+		if (mesh_handle.IsValid())
+		{
+			auto modelData = mesh_handle.GetData<ModelData*>();
+			id = id % modelData->gfxMeshIndices.size();
+			submodel_id = id % modelData->gfxMeshIndices.size();
+			model_handle = modelData->gfxMeshIndices[submodel_id];
+		}
 	}
 }
