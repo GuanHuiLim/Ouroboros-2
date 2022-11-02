81aa602d185269ca2d59e56a04614e2474fd7a98
Lim Guan Hui <l.guanhui@digipen.edu.sg>
2022-11-02 12:38:21 +0800

    skin mesh rendering works

--- Editor/src/Ouroboros/Vulkan/SkinRendererComponent.cpp
+++ Editor/src/Ouroboros/Vulkan/SkinRendererComponent.cpp
@@ -5,10 +5,4 @@
 
 namespace oo
 {
-	RTTR_REGISTRATION
-	{
-		using namespace rttr;
-	registration::class_<SkinMeshRendererComponent>("SkinMeshRendererComponent")
-		.property_readonly("Mesh Handle", &SkinMeshRendererComponent::mesh_handle);
-	};
 }
