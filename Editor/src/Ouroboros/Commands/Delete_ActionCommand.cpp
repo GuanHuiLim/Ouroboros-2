0e0bb35e5506adf329dd9d684e8cf11e8f5e2519
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2022-11-02 18:06:06 +0800

    able to stringifiy the command

--- Editor/src/Ouroboros/Commands/Delete_ActionCommand.cpp
+++ Editor/src/Ouroboros/Commands/Delete_ActionCommand.cpp
@@ -4,11 +4,13 @@
 #include "App/Editor/Utility/ImGuiManager.h"
 #include "Ouroboros/Scene/Scene.h"
 #include "SceneManagement/include/SceneManager.h"
+
 oo::Delete_ActionCommand::Delete_ActionCommand(std::shared_ptr<oo::GameObject> deletedObj)
 	:data { Serializer::SaveDeletedObject(deletedObj, *ImGuiManager::s_scenemanager->GetActiveScene<oo::Scene>()) }
 {
 	message = "Delete Object :" + deletedObj->Name();
 	parentID = deletedObj->GetParentUUID();
+	revivedObject = deletedObj->GetInstanceID();
 }
 
 oo::Delete_ActionCommand::~Delete_ActionCommand()
@@ -35,6 +37,21 @@ void oo::Delete_ActionCommand::Redo()
 	if (obj == nullptr)
 		ASSERT_MSG(true, "object not found");
 	scene->DestroyGameObject(*obj);
+}
+oo::Delete_ActionCommand::Delete_ActionCommand(std::string& _data)
+{
+	message = "Delete Object : ";
+	size_t offset = _data.find(',');
+	parentID = std::stoull(std::string(_data.begin(), _data.begin() + offset));
+	data = std::string(_data.begin() + (offset+1), _data.end());
+}
+std::string oo::Delete_ActionCommand::GetData()
+{
+	auto piD = parentID.GetUUID();
+	std::string currData = std::to_string(piD);//its ok
+	currData += ',';
+	currData += data;
+	return currData;
 }
 
 oo::Create_ActionCommand::Create_ActionCommand(std::shared_ptr<oo::GameObject> createdobj)
--- Editor/src/Ouroboros/Commands/Delete_ActionCommand.h
+++ Editor/src/Ouroboros/Commands/Delete_ActionCommand.h
@@ -7,11 +7,14 @@ namespace oo
 	class Delete_ActionCommand:public ActionCommand
 	{
 	public:
+		Delete_ActionCommand(std::string& data);
 		Delete_ActionCommand(std::shared_ptr<oo::GameObject> deletedObj);
 		~Delete_ActionCommand();
 
 		void Undo()override;
 		void Redo()override;
+		std::string GetData();
+		
 	private:
 		oo::UUID parentID;
 		oo::UUID revivedObject = oo::UUID(static_cast<uint64_t>(-1));
