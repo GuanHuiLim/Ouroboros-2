c6d59f8548d988df8903b7486d80fb3674fb8d2c
DeveloperSolo <dragonloversol@gmail.com>
2022-11-17 00:27:08 +0800

    added C# Physics.Raycast

---
+++ Editor/vendor/scriptcore/src/ScriptCore/Data/RaycastHit.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ouroboros
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RaycastHit
+    {
+        public Transform transform;
+        public Rigidbody rigidbody;
+        public Collider collider;
+
+        public Vector3 point;
+        public Vector3 normal;
+        public float distance;
+    }
+}
