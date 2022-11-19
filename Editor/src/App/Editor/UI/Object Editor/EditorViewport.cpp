0d85facbab8f3ce15645f2d3e6607e714a092bf6
JunXiang-Leong <72239762+JunXiang-Leong@users.noreply.github.com>
2022-11-19 21:52:53 +0800

    fixed some of the minor issues with ouroboros collab

--- Editor/src/App/Editor/UI/Object Editor/EditorViewport.cpp
+++ Editor/src/App/Editor/UI/Object Editor/EditorViewport.cpp
@@ -109,8 +109,7 @@ void EditorViewport::Show()
 				LOG_TRACE("valid graphics ID from picking {0}", graphicsID);
 				auto uuid = scene->GetUUIDFromGraphicsId(graphicsID); //scene->GetWorld().Get_System<oo::RendererSystem>()->GetUUID(graphicsID);
 				ASSERT_MSG(uuid == oo::UUID::Invalid, " Attempting to pick on an object with invalid uuid {0}, this should not occur at this point!!!", uuid ); 
-				Hierarchy::GetSelectedNonConst().clear();
-				Hierarchy::GetSelectedNonConst().emplace(uuid);
+				Hierarchy::SetItemSelected(uuid);
 			}
 		}
 	}
