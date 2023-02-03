44e86525f3daac00342154853e2f607166087ad3
DeveloperSolo <dragonloversol@gmail.com>
2023-02-04 02:02:46 +0800

    forgot ColliderAPI comments

--- Editor/src/Ouroboros/API/ColliderAPI.h
+++ Editor/src/Ouroboros/API/ColliderAPI.h
@@ -1,3 +1,17 @@
+/************************************************************************************//*!
+\file           ColliderAPI.h
+\project        Ouroboros
+\author         Solomon Tan Teng Shue, t.tengshuesolomon, 620010020 | code contribution (100%)
+\par            email: t.tengshuesolomon\@digipen.edu
+\date           Nov 4, 2022
+\brief          Defines the exported helper functions that the C# scripts will use
+                to interact with the C++ Collider ECS Components (e.g. BoxCollider, SphereCollider, etc)
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
 #pragma once
 #include "Ouroboros/Scripting/ExportAPI.h"
 #include "Ouroboros/Scripting/ScriptManager.h"
