d09bf85978661109d81df6a1673e94a1ebb4499a
DeveloperSolo <dragonloversol@gmail.com>
2023-03-22 13:51:01 +0800

    added C# emissiveColor to MeshRenderer and SkinnedMeshRenderer

--- Editor/vendor/scriptcore/src/ScriptCore/Components/MeshRenderer.cs
+++ Editor/vendor/scriptcore/src/ScriptCore/Components/MeshRenderer.cs
@@ -1,9 +1,17 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Ouroboros
 {
     public class MeshRenderer : Component
     {
+        [DllImport("__Internal")] private static extern Color MeshRendererComponent_GetEmissiveColor(UInt32 sceneID, UInt64 instanceID);
+        [DllImport("__Internal")] private static extern void MeshRendererComponent_SetEmissiveColor(UInt32 sceneID, UInt64 instanceID, Color value);
 
+        public Color emissiveColor
+        {
+            get { return MeshRendererComponent_GetEmissiveColor(gameObject.scene, gameObject.GetInstanceID()); }
+            set { MeshRendererComponent_SetEmissiveColor(gameObject.scene, gameObject.GetInstanceID(), value); }
+        }
     }
 }
---
+++ Editor/vendor/scriptcore/src/ScriptCore/Components/SkinnedMeshRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Ouroboros
+{
+    public class SkinnedMeshRenderer : Component
+    {
+        [DllImport("__Internal")] private static extern Color SkinMeshRendererComponent_GetEmissiveColor(UInt32 sceneID, UInt64 instanceID);
+        [DllImport("__Internal")] private static extern void SkinMeshRendererComponent_SetEmissiveColor(UInt32 sceneID, UInt64 instanceID, Color value);
+
+        public Color emissiveColor
+        {
+            get { return SkinMeshRendererComponent_GetEmissiveColor(gameObject.scene, gameObject.GetInstanceID()); }
+            set { SkinMeshRendererComponent_SetEmissiveColor(gameObject.scene, gameObject.GetInstanceID(), value); }
+        }
+    }
+}
