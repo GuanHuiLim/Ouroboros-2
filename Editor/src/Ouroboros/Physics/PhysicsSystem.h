c9e7a79571c2f057886a89097066856405afbe09
Chua Teck Lee <C_tecklee@Hotmail.com>
2023-03-26 01:18:46 +0800

    final push

--- Editor/src/Ouroboros/Physics/PhysicsSystem.h
+++ Editor/src/Ouroboros/Physics/PhysicsSystem.h
@@ -67,10 +67,10 @@ namespace oo
     
     private:
         //inline static std::uint64_t MaxIterations = 2;
-        inline static Timestep FixedDeltaTimeBase = 1.0/100.0;               // physics updates at 60 fps
+        inline static Timestep FixedDeltaTimeBase = 1.0/60.0;               // physics updates at 60 fps
         inline static Timestep FixedDeltaTimescale = 1.0;                  // additional level of control for scripts
         inline static Timestep FixedDeltaTime = FixedDeltaTimeBase * FixedDeltaTimescale;  // physics updates at 60 fps
-        inline static Timestep MaxFrameRateMultiplier = 2;
+        inline static Timestep MaxFrameRateMultiplier = 3;
         inline static Timestep MaxFrameTime = FixedDeltaTime * MaxFrameRateMultiplier;
         //inline static Timestep AccumulatorLimit = FixedDeltaTime * MaxIterations;   // To prevent spiral of death
 
