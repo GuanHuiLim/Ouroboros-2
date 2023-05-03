145db151fc2bd9b2a8c9900d6fea897bd6b8c203
Chua Teck Lee <C_tecklee@Hotmail.com>
2023-05-03 23:44:54 +0800

    attempt at parallizing anims and ran into few issues

--- Editor/src/Ouroboros/Animation/AnimationSystem.cpp
+++ Editor/src/Ouroboros/Animation/AnimationSystem.cpp
@@ -95,20 +95,24 @@ namespace oo::Anim
 					.SetParameter("Test float", 30.f);
 			}
 		}*/
-		
-		TRACY_PROFILE_SCOPE_NC(Animation_Update, 0x00E0E3);
 
-		world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) {
-			GameObject go{ entity , *scene };
-			auto& anim_component = animationComp.GetActualComponent();
-			internal::UpdateTrackerInfo info{ *this,anim_component,animationComp.GetTracker(), entity,go.GetInstanceID(), timer::dt() };
-			internal::UpdateTracker(info);
-			if (anim_component.tracker.transition_info.in_transition)
+		static Ecs::Query animationQuery = Ecs::make_raw_query<oo::GameObjectComponent, oo::AnimationComponent>();
+
+		TRACY_PROFILE_SCOPE_NC(Animation_Update, 0x00E0E3);
+		
+		//world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) 
+		m_world->parallel_for_each(animationQuery, [&](oo::GameObjectComponent& goc, oo::AnimationComponent& animationComp)
 			{
-				animationComp.GetActualComponent().skeleton.Apply_NextPose_To_Gameobjects(*scene);
-			}
-			else
-				animationComp.GetActualComponent().skeleton.Apply_CurrentPose_To_Gameobjects(*scene);
+				auto go = scene->FindWithInstanceID(goc.Id);
+				auto& anim_component = animationComp.GetActualComponent();
+				internal::UpdateTrackerInfo info{ *this,anim_component,animationComp.GetTracker(), go->GetEntity(), go->GetInstanceID(), timer::dt() };
+				internal::UpdateTracker(info);
+				if (anim_component.tracker.transition_info.in_transition)
+				{
+					animationComp.GetActualComponent().skeleton.Apply_NextPose_To_Gameobjects(*scene);
+				}
+				else
+					animationComp.GetActualComponent().skeleton.Apply_CurrentPose_To_Gameobjects(*scene);
 			});
 		TRACY_PROFILE_SCOPE_END();
 		/*world->for_each(query, [&](AnimationComponent& animationComp) {
@@ -136,12 +140,14 @@ namespace oo::Anim
 			internal::ReloadReferences(tree);
 		}
 
-		world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) {
-
-			animationComp.Set_Root_Entity(entity);
+		static Ecs::Query animationQuery = Ecs::make_raw_query<oo::GameObjectComponent, oo::AnimationComponent>();
+		//world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) 
+		m_world->parallel_for_each(animationQuery, [&](oo::GameObjectComponent& goc, oo::AnimationComponent& animationComp)
+		{
+			auto go = scene->FindWithInstanceID(goc.Id);
+			animationComp.Set_Root_Entity(go->GetEntity());
 			internal::InitialiseComponent(animationComp.GetActualComponent());
-
-			});
+		});
 
 		
 
@@ -369,14 +375,18 @@ namespace oo::Anim
 			return _query;
 		}();
 		
-		world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) {
+		//world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) 
+		static Ecs::Query animationQuery = Ecs::make_raw_query<oo::GameObjectComponent, oo::AnimationComponent>();
+		//world->for_each_entity_and_component(query, [&](Ecs::EntityID entity, oo::AnimationComponent& animationComp) 
+		m_world->parallel_for_each(animationQuery, [&](oo::GameObjectComponent& goc, oo::AnimationComponent& animationComp)
+		{
+			auto go = scene->FindWithInstanceID(goc.Id);
+			if (animationComp.GetActualComponent().root_objectID.value == go->GetEntity().value) return;
 			
-			if (animationComp.GetActualComponent().root_objectID.value == entity.value) return;
-			
-			animationComp.Set_Root_Entity(entity);
+			animationComp.Set_Root_Entity(go->GetEntity());
 			internal::InitialiseComponent(animationComp.GetActualComponent());
 
-			});
+		});
 	}
 
 	bool AnimationSystem::SaveAnimationTree(AnimationTree& tree, std::string filepath)
