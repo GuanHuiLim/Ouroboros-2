e80d5584db766e02ef42d23c97096acb01254799
Chua Teck Lee <C_tecklee@Hotmail.com>
2023-05-27 14:11:07 +0800

    edited various scripts premake and batch files to copy the right information for a clean build

--- scripts/compile_executable.bat
+++ scripts/compile_executable.bat
@@ -1,2 +1,12 @@
+@echo off
+pushd %~dp0\..\bin\Production-Executable-windows-x86_64\Editor
+echo "Cleaning executable file location"
+call CleanUp-Exe.bat
+echo "Cleaning Complete"
+popd
+
+echo "Building Exe"
 "..\Editor\Inno Setup 6\ISCC.exe" /Qp ..\bin\Production-Executable-windows-x86_64\Editor\executable_portable.iss
+echo "Build Complete"
+
 PAUSE
